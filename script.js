1325d1d7768cece50811ba8fac0f64990b4d9311
Panda <106486268+C0ding-Panda@users.noreply.github.com>
2022-09-14 15:44:26 +0200

    pdf, link

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function pdfanzeigen(){
     else{
         document.getElementById("pdf-img").style.display = "none";
         document.getElementById("pdf-img").style.width = "35vw";
-        document.getElementById("pdf-img").style.height = "50vw";
+        document.getElementById("pdf-img").style.height = "110vw";
         document.getElementById("download-btn").style.display = "none";
         document.getElementById("pdf-btn").innerHTML = "PDF einblenden";
         anzahlKlicks = 0;
@@ -31,31 +31,31 @@ function klicksMessen() {
         switch(anzahlKlicks) {
             case -1:
                 document.getElementById("pdf-img").style.width = "30vw";
-                document.getElementById("pdf-img").style.height = "85vw";
+                document.getElementById("pdf-img").style.height = "100vw";
                 break;
             case 0:
                 document.getElementById("pdf-img").style.width = "35vw";
-                document.getElementById("pdf-img").style.height = "90vw";
+                document.getElementById("pdf-img").style.height = "110vw";
                 break;
             case 1:
                 document.getElementById("pdf-img").style.width = "40vw";
-                document.getElementById("pdf-img").style.height = "95vw";
+                document.getElementById("pdf-img").style.height = "120vw";
                 break;
             case 2:
                 document.getElementById("pdf-img").style.width = "45vw";
-                document.getElementById("pdf-img").style.height = "100vw";
+                document.getElementById("pdf-img").style.height = "130vw";
                 break;
             case 3:
-                document.getElementById("pdf-img").style.width = "50vw";
-                document.getElementById("pdf-img").style.height = "110vw";
+                document.getElementById("pdf-img").style.width = "55vw";
+                document.getElementById("pdf-img").style.height = "150vw";
                 break;
             case 4:
-                document.getElementById("pdf-img").style.width = "60vw";
-                document.getElementById("pdf-img").style.height = "120vw";
+                document.getElementById("pdf-img").style.width = "65vw";
+                document.getElementById("pdf-img").style.height = "170vw";
                 break;
             case 5:
                 document.getElementById("pdf-img").style.width = "80vw";
-                document.getElementById("pdf-img").style.height = "150vw";
+                document.getElementById("pdf-img").style.height = "190vw";
                 break;   
         }
         if (anzahlKlicks < -1){
